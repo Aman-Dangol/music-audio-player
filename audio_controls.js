f4cc8806f7813d86dbbf26683ee369bcc3e3048b
Aman Dangol <amandangol423@gmail.com>
2023-06-12 18:55:20 +0545

    made small changes

--- audio_controls.js
+++ audio_controls.js
@@ -1,10 +1,9 @@
-let audio="";
+let audio ="";
 let file = "";
 let fromFile="";
 let url="";
-
-function displayFile(){
-    audio = document.getElementById('audio')
+function playMusic(){
+    audio = document.getElementById('audio');
     file = document.getElementById('file');
     fromFile=file.files[0];
     url= URL.createObjectURL(fromFile)
